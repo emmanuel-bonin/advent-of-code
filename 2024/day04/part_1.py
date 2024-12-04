214b0151e99e20f43a258276b65f7678bc4f88ec
Emmanuel BONIN <emmanuel@bodyguard.ai>
2024-12-04 06:54:31 +0000

    refacto: day 4 part 1

--- 2024/day04/part_1.py
+++ 2024/day04/part_1.py
@@ -2,13 +2,12 @@ input_file = open("./input.txt", "r")
 lines = input_file.readlines()
 
 grid = []
-words = []
 
 for line in lines:
   for i in range(len(line.split())):
     grid.append(line.split()[i])
 
-def count_xmas_in_direction(grid, i, j, direction):
+def count_xmas_in_direction(grid, i, j):
   return ((1 if i>2 and j>2 and grid[i-1][j-1] == 'M' and grid[i-2][j-2] == 'A' and grid[i-3][j-3] == 'S' else 0) +
          (1 if i>2 and grid[i-1][j] == 'M' and grid[i-2][j] == 'A' and grid[i-3][j] == 'S' else 0) +
          (1 if i>2 and j<len(grid[i])-3 and grid[i-1][j+1] == 'M' and grid[i-2][j+2] == 'A' and grid[i-3][j+3] == 'S' else 0) +
@@ -22,5 +21,5 @@ result = 0
 for i in range(len(grid)):
   for j in range(len(grid[i])):
     if grid[i][j] == 'X':
-      result += count_xmas_in_direction(grid, i, j, 'X')
+      result += count_xmas_in_direction(grid, i, j)
 print(result)
