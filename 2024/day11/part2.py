f1129f40cb18a07bd0e4abfbb3afb919060c9075
Emmanuel BONIN <emmanuel@bodyguard.ai>
2024-12-11 09:32:57 +0000

    refacto: clean code

--- 2024/day11/part2.py
+++ 2024/day11/part2.py
@@ -11,7 +11,7 @@ def trim_leading(num):
   return res
 
 # Map containing the result computed for each number and times
-map = {}
+cache = {}
 
 def blink(n, times):
   if times == 0:
@@ -21,15 +21,20 @@ def blink(n, times):
     if n == '0':
       n = '1'
     elif len(n) % 2 == 0:
+      # Split the number in half and keeping the second one
       half = trim_leading(n[int(len(n)/2):])
+      # Set the first half in n for next iteration
       n = n[0:int(len(n)/2)]
-      if half in map is not None and times-(i+1) in map[half] is not None:
-        res += map[half][times-(i+1)]
+
+      # If the result for the half and times-(i+1) is already computed, use it
+      if half in cache is not None and times-(i+1) in cache[half] is not None:
+        res += cache[half][times-(i+1)]
       else:
+        # Otherwise, compute it and store it in the cache
         a = blink(half, times-(i+1))
-        if half not in map:
-          map[half] = {}
-        map[half][times-(i+1)] = a
+        if half not in cache:
+          cache[half] = {}
+        cache[half][times-(i+1)] = a
         res += a
     else:
       n = str(int(n) * 2024)
