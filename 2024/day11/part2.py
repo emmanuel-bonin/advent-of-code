a4871e016904d2d5646f056fe7de67640040ee35
Emmanuel BONIN <emmanuel@bodyguard.ai>
2024-12-11 09:31:45 +0000

    refacto

--- 2024/day11/part2.py
+++ 2024/day11/part2.py
@@ -21,16 +21,15 @@ def blink(n, times):
     if n == '0':
       n = '1'
     elif len(n) % 2 == 0:
-      h1 = n[0:int(len(n)/2)]
-      h2 = trim_leading(n[int(len(n)/2):])
-      n = h1
-      if h2 in map is not None and times-(i+1) in map[h2] is not None:
-        res += map[h2][times-(i+1)]
+      half = trim_leading(n[int(len(n)/2):])
+      n = n[0:int(len(n)/2)]
+      if half in map is not None and times-(i+1) in map[half] is not None:
+        res += map[half][times-(i+1)]
       else:
-        a = blink(h2, times-(i+1))
-        if h2 not in map:
-          map[h2] = {}
-        map[h2][times-(i+1)] = a
+        a = blink(half, times-(i+1))
+        if half not in map:
+          map[half] = {}
+        map[half][times-(i+1)] = a
         res += a
     else:
       n = str(int(n) * 2024)
