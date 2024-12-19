02577b33b89a5a2b8737cc4f53f103a3abe9d186
Emmanuel BONIN <emmanuel@bodyguard.ai>
2024-12-19 13:57:56 +0000

    chore: day19 part2

--- 2024/day19/part2.py
+++ 2024/day19/part2.py
@@ -1,38 +1,33 @@
+from functools import *
+
 f = open('input.txt', 'r')
 lines = f.readlines()
 f.close()
 
-patterns = lines[0].strip('\n').split(' ')
-for i in range(len(patterns)):
-  patterns[i] = patterns[i].strip(',')
+raw_patterns = lines[0].strip('\n').split(' ')
+for i in range(len(raw_patterns)):
+  raw_patterns[i] = raw_patterns[i].strip(',')
+patterns = tuple(x for x in raw_patterns)
 lines = lines[2:]
 for i in range(len(lines)):
   lines[i] = lines[i].strip('\n')
 
 cache = {}
 
+@lru_cache(maxsize=None)
 def match_pattern(patterns, line):
+  result = 0
   if len(line) == 0:
-    return True
+    return 1
   else:
     for i in range(0, len(patterns)):
       if line.startswith(patterns[i]):
-        if patterns[i] in cache:
-          if line[len(patterns[i]):] in cache[patterns[i]]:
-            return cache[patterns[i]][line[len(patterns[i]):]]
-        if match_pattern(patterns, line[len(patterns[i]):]):
-          if not patterns[i] in cache:
-            cache[patterns[i]] = {}
-          cache[patterns[i]] = {line[len(patterns[i]):]: True}
-          return True
-        else:
-          if not patterns[i] in cache:
-            cache[patterns[i]] = {}
-          cache[patterns[i]] = {line[len(patterns[i]):]: False}
-    return False
+        result += match_pattern(patterns, line[len(patterns[i]):])
+  return result
 
 count = 0
 for line in lines:
-  if match_pattern(patterns, line):
-    count += 1
+  s = match_pattern(patterns, line)
+  if s > 0:
+    count += s
 print(count)
