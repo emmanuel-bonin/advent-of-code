94a30775f5500ccfeb727d6edf08e01c0863d828
Emmanuel BONIN <emmanuel@bodyguard.ai>
2024-12-20 07:14:11 +0000

    still trying to nail it

--- 2024/day16/part1.py
+++ 2024/day16/part1.py
@@ -28,13 +28,24 @@ for y in range(len(lines)):
       end_x = x
       end_y = y
     elif lines[y][x] == '.':
+      if ((lines[y-1][x] == '.' and lines[y-2][x] != '.') or
+         (lines[y+1][x] == '.' and lines[y+2][x] != '.') or
+         (lines[y][x-1] == '.' and lines[y][x-2] != '.') or
+         (lines[y][x+1] == '.' and lines[y][x+2] != '.')):
+        raw_matrix[y].append(1000)
+      else:
+        raw_matrix[y].append(1)
       map[y].append('.')
-      raw_matrix[y].append(1)
     elif lines[y][x] == '#':
       map[y].append('#')
       raw_matrix[y].append(0)
 
 f.close()
+
+# for i in range(len(raw_matrix)):
+#   for j in range(len(raw_matrix[i])):
+#     print('\t' + str(raw_matrix[i][j]), end='')
+#   print()
 
 def debug_map(map, path):
   s = ''
@@ -51,71 +62,45 @@ def debug_map(map, path):
         s += map[a][b]
     s += '\n'
   print(s)
+grid = Grid(matrix=np.array(raw_matrix))
 
-final_result = 2147483647
-final_path = []
-rerun = True
-# matrices = [raw_matrix]
-while rerun is True:
-  rerun = False
-#   raw_matrix = matrices.pop(0)
-#   print('111 popped matrix', len(matrices))
-  new_matrix = np.array(raw_matrix)
-  grid = Grid(matrix=new_matrix)
+start = grid.node(start_x, start_y)
+end = grid.node(end_x, end_y)
 
-  start = grid.node(start_x, start_y)
-  end = grid.node(end_x, end_y)
+finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+path, runs = finder.find_path(start, end, grid)
 
-  finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
-  path, runs = finder.find_path(start, end, grid)
+n_turn = 0
+n_forward = 0
+prev_dir = 'R'
+old_x = 0
+old_y = 0
+diff_x = 0
+diff_y = 0
+result = 0
+for i in range(len(path)):
+  x, y = path[i]
+  if i != 0:
+    diff_x = x - old_x
+    diff_y = y - old_y
+    cur_dir = None
+    if diff_x == 1:
+      cur_dir = 'R'
+    elif diff_x == -1:
+      cur_dir = 'L'
+    elif diff_y == 1:
+      cur_dir = 'D'
+    elif diff_y == -1:
+      cur_dir = 'U'
 
-  n_turn = 0
-  n_forward = 0
-  prev_dir = 'R'
-  old_x = 0
-  old_y = 0
-  diff_x = 0
-  diff_y = 0
-  for i in range(len(path)):
-    x, y = path[i]
-    if i != 0:
-      diff_x = x - old_x
-      diff_y = y - old_y
-      cur_dir = None
-      if diff_x == 1:
-        cur_dir = 'R'
-      elif diff_x == -1:
-        cur_dir = 'L'
-      elif diff_y == 1:
-        cur_dir = 'D'
-      elif diff_y == -1:
-        cur_dir = 'U'
-
-      n_forward += 1
-      if prev_dir != cur_dir:
-        if raw_matrix[old_y][old_x] == 1:
-          raw_matrix[old_y][old_x] = 1000
-          rerun = True
-#           matrices.append(raw_matrix.copy())
-#           print('222 pushing new matrix', len(matrices))
-        n_turn += 1
-      else:
-        if raw_matrix[old_y][old_x] == 1000:
-          raw_matrix[old_y][old_x] = 1
-          rerun = True
-#           matrices.append(raw_matrix.copy())
-#           print('333 pushing new matrix', len(matrices))
-      prev_dir = cur_dir
-      result = n_forward + n_turn * 1000
-      if result > final_result:
-        break
-    old_x = x
-    old_y = y
-#   print(len(matrices), result)
-  if result < final_result:
-    final_result = result
-    print('=>', final_result)
-    final_path = path
+    result += 1
+    n_forward += 1
+    if prev_dir != cur_dir:
+      result += 1000
+      n_turn += 1
+    prev_dir = cur_dir
+  old_x = x
+  old_y = y
 
 # debug_map(map, path)
-print('=>', final_result)
+print('=>', n_turn, n_forward, n_turn * 1000 + n_forward, result)
