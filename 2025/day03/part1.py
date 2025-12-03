12aaf85a2e84e802b0de6ab464d3949e798490ac
Emmanuel BONIN <emmanuel@bodyguard.ai>
2025-12-03 08:31:21 +0000

    refacto: 2025 day03

--- 2025/day03/part1.py
+++ 2025/day03/part1.py
@@ -1,12 +1,11 @@
 f = open('input.txt', 'r')
 lines = f.readlines()
 
-def get_greater_num_idx(s, go_to_end=False, start_idx=-1):
+def get_greater_num_idx(s):
     cur = 0
     res = 0
-    max_idx = len(s) if go_to_end else len(s)-1
-    i = 0 if start_idx == -1 else start_idx+1
-    while i < max_idx:
+    i = 0
+    while i < len(s):
         if int(s[i]) > cur:
             cur = int(s[i])
             res = i
@@ -16,8 +15,8 @@ def get_greater_num_idx(s, go_to_end=False, start_idx=-1):
 res = 0
 for line in lines:
     line = line.strip()
-    idx1 = get_greater_num_idx(line, go_to_end=False)
-    idx2 = get_greater_num_idx(line, go_to_end=True, start_idx=idx1)
+    idx1 = get_greater_num_idx(line[:len(line)-1])
+    idx2 = get_greater_num_idx(line[idx1+1:]) + idx1+1
     res += int(line[idx1]+line[idx2])
 
 print(res)
--- 2025/day03/part2.py
+++ 2025/day03/part2.py
@@ -1,12 +1,11 @@
 f = open('input.txt', 'r')
 lines = f.readlines()
 
-def get_greater_num_idx(s, start=-1, margin_end=-1):
+def get_greater_num_idx(s):
     cur = 0
     res = 0
-    max_idx = len(s) if margin_end == -1 else len(s)-margin_end
-    i = start + 1
-    while i <= max_idx:
+    i = 0
+    while i < len(s):
         if int(s[i]) > cur:
             cur = int(s[i])
             res = i
@@ -17,10 +16,16 @@ res = 0
 for line in lines:
     line = line.strip()
     idx_arr = []
-    for i in range(12):
-        r = get_greater_num_idx(line, start=idx_arr[len(idx_arr)-1] if len(idx_arr) > 0 else -1, margin_end=12-len(idx_arr))
-        idx_arr.append(r)
+    i = 0
+    while i < 12:
+        prev_idx = (idx_arr[len(idx_arr)-1] if len(idx_arr) > 0 else -1) + 1
+        # Create a substring from previous found index (or 0 if first iteration) to length of line minus remaining numbers to find to reach 12
+        # and get index of greater number in this string
+        r = get_greater_num_idx(line[prev_idx:len(line)-(11-len(idx_arr))])
+        idx_arr.append(r+prev_idx)
+        i += 1
     res_s = ''
+    # Create the number by concatenating numbers at each found indexes
     for i in idx_arr:
         res_s += line[i]
     res += int(res_s)
