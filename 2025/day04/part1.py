abfdcf5dc108b584f64a1bda352f85bd724a4c4d
Emmanuel BONIN <emmanuel@bodyguard.ai>
2025-12-04 06:42:45 +0000

    2025 day04

---
+++ 2025/day04/part1.py
@@ -0,0 +1,28 @@
+f = open("input.txt", "r")
+lines = [line.strip() for line in f.readlines()]
+
+def count_nearby_rolls(_lines: list[str], _x: int, _y: int):
+    max_x = len(_lines[0])-1
+    max_y = len(_lines)-1
+    return (
+        (0 if _y-1 < 0 or _x-1 < 0 else 1 if _lines[_y-1][_x-1] != '.' else 0) +
+        (0 if _y-1 < 0 else 1 if _lines[_y-1][_x] != '.' else 0) +
+        (0 if _y-1 < 0 or _x+1 > max_x else 1 if _lines[_y-1][_x+1] != '.' else 0) +
+        (0 if _x-1 < 0 else 1 if _lines[_y][_x-1] != '.' else 0) +
+        (0 if _x+1 > max_x else 1 if _lines[_y][_x+1] != '.' else 0) +
+        (0 if _y+1 > max_y or _x-1 < 0 else 1 if _lines[_y+1][_x-1] != '.' else 0) +
+        (0 if _y+1 > max_y else 1 if _lines[_y+1][_x] != '.' else 0) +
+        (0 if _y+1 > max_y or _x+1 > max_x else 1 if _lines[_y+1][_x+1] != '.' else 0)
+    )
+
+res = 0
+for y in range(len(lines)):
+    for x in range(len(lines[y])):
+        if lines[y][x] == "@":
+            adj_rolls = count_nearby_rolls(lines, x, y)
+            if adj_rolls < 4:
+                # For debugging purpose
+                lines[y] = lines[y][:x] + 'x' + lines[y][x+1:]
+                res += 1
+
+print(res)
