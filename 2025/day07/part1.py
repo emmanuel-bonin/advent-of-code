07e2e5e7dd50185ab8fb4b591c9b4bdad3bbd5f0
Emmanuel BONIN <emmanuel.blanc.bonin@gmail.com>
2025-12-07 20:58:37 +0100

    2025 day 7 part 1

---
+++ 2025/day07/part1.py
@@ -0,0 +1,22 @@
+f = open('input.txt', 'r')
+lines = [line.strip() for line in f.readlines()]
+
+rays_x = set()
+res = 0
+for y in range(len(lines)):
+    for x in range(len(lines[y])):
+        if lines[y][x] == 'S':
+            rays_x.add(x)
+            continue
+        if lines[y][x] == '^':
+            done = False
+            while not done:
+                done = True
+                for rx in rays_x:
+                    if rx == x:
+                        res += 1
+                        rays_x.add(x-1)
+                        rays_x.add(x+1)
+                        rays_x.remove(x)
+                        break
+print(res)
